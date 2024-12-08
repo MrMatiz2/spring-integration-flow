4da542447730aeaa478cc4a81196a79f6486a85b
MrMatiz2 <alejandromatiz87@gmail.com>
2024-12-08 17:39:10 -0500

    Initial commit

---
+++ src/main/java/com/example/springintegrationflow/gateways/FileWriterGateway.java
@@ -0,0 +1,15 @@
+package com.example.springintegrationflow.gateways;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "textInChannel")
+public interface FileWriterGateway {
+
+    void writeToFile(
+        @Header(FileHeaders.FILENAME) String fileName,
+        String data
+    );
+
+}
---
+++ src/test/java/com/example/springintegrationflow/SpringIntegrationFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintegrationflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
